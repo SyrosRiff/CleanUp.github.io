0f604949510a3e0e1e1705b9d37fde1056d20446
Lauren Hong <114040016+LH-eliza@users.noreply.github.com>
2022-11-21 14:33:59 -0500

    Delete Main.js

--- Main.js
+++
@@ -1,4 +0,0 @@
-//JavaScript for Our Impact
-<button class="button" type="button"
-          onclick="document.getElementById('demo').innerHTML='Joined!'">JOIN OUR COMMUNITY</button>
-          <p id="demo"></p>
